1917637f8c2515eb9571258eb1f6f16349344adc
Gilberto Mascena <gilbertomascena@gmail.com>
2025-06-04 11:04:39 -0300

    :sparkles: feat: Implemented the Contact class

--- src/main/java/br/com/mascenadev/projetoagendaspringboot/entities/Contato.java
+++ src/main/java/br/com/mascenadev/projetoagendaspringboot/entities/Contato.java
@@ -1,4 +1,76 @@
 package br.com.mascenadev.projetoagendaspringboot.entities;
 
-public class Contato {
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Contato implements Serializable {
+    private static final long serializableId = 1L;
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+
+    public Contato() {
+    }
+
+    public Contato(String nome, String email, String telefone) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Contato contato = (Contato) o;
+        return Objects.equals(id, contato.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Contato{" +
+               "id=" + id +
+               ", nome='" + nome + '\'' +
+               ", email='" + email + '\'' +
+               ", telefone='" + telefone + '\'' +
+               '}';
+    }
 }
