c55db2e7c66b6fca083056bc86cea4b843e20c26
Gilberto Mascena <gilbertomascena@gmail.com>
2025-06-04 16:41:59 -0300

    :pencil: Updated the message of the Contact Not Found Exception class

--- src/main/java/br/com/mascenadev/projetoagendaspringboot/exception/ContatoNaoEncontradoException.java
+++ src/main/java/br/com/mascenadev/projetoagendaspringboot/exception/ContatoNaoEncontradoException.java
@@ -2,7 +2,7 @@ package br.com.mascenadev.projetoagendaspringboot.exception;
 
 public class ContatoNaoEncontradoException extends RuntimeException {
     public ContatoNaoEncontradoException(Long id) {
-        super("Contato não encontrado com o ID:" + id);
+        super("Contato com o ID:" + id + " não foi encontrado");
     }
 
     public ContatoNaoEncontradoException() {
