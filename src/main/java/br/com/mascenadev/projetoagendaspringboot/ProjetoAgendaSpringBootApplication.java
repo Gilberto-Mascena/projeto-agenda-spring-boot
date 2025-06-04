70440ee82bc0a0c8b3d7f5921e8c5583d1feb55b
Gilberto Mascena <gilbertomascena@gmail.com>
2025-06-04 10:33:24 -0300

    :tada: init: project created

---
+++ src/main/java/br/com/mascenadev/projetoagendaspringboot/ProjetoAgendaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.mascenadev.projetoagendaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAgendaSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoAgendaSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/mascenadev/projetoagendaspringboot/ProjetoAgendaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mascenadev.projetoagendaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoAgendaSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
