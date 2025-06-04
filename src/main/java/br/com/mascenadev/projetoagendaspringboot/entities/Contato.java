1b5eb0ec83a5b9766c8678caf1e67c1add358b89
Gilberto Mascena <gilbertomascena@gmail.com>
2025-06-04 11:33:43 -0300

    :sparkles: feat: Added Annotations for jpa mapping and validations

--- src/main/java/br/com/mascenadev/projetoagendaspringboot/entities/Contato.java
+++ src/main/java/br/com/mascenadev/projetoagendaspringboot/entities/Contato.java
@@ -1,14 +1,37 @@
 package br.com.mascenadev.projetoagendaspringboot.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class Contato implements Serializable {
     private static final long serializableId = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "Nome é obrigatório")
+    @Size(min = 3, max = 100, message = "Nome deve ter entre 3 e 100 caracteres")
     private String nome;
+
+    @NotBlank(message = "Email é obrigatório")
+    @Email(message = "Email inválido")
     private String email;
+
+    @NotBlank(message = "Telefone é obrigatório")
+    @Pattern(
+            regexp = "\\(\\d{2}\\) \\d{4,5}-\\d{4}",
+            message = "Telefone deve estar no formato (XX) XXXXX-XXXX ou (XX) XXXX-XXXX")
     private String telefone;
 
     public Contato() {
