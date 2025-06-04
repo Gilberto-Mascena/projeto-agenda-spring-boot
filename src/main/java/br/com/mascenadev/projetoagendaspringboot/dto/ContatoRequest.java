236e7149977841db0e7f2c1f96d2e5f4a486fa93
Gilberto Mascena <gilbertomascena@gmail.com>
2025-06-04 13:14:05 -0300

    :sparkles: feat: Implemented ContactRequest

--- src/main/java/br/com/mascenadev/projetoagendaspringboot/dto/ContatoRequest.java
+++ src/main/java/br/com/mascenadev/projetoagendaspringboot/dto/ContatoRequest.java
@@ -1,4 +1,58 @@
 package br.com.mascenadev.projetoagendaspringboot.dto;
 
+import br.com.mascenadev.projetoagendaspringboot.entities.Contato;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.beans.BeanUtils;
+
 public class ContatoRequest {
+
+    @NotBlank(message = "Nome é obrigatório")
+    private String nome;
+
+    @NotBlank(message = "Email é obrigatório")
+    @Email(message = "Email inválido")
+    private String email;
+
+    @NotBlank(message = "Telefone é obrigatório")
+    private String telefone;
+
+    public ContatoRequest() {
+    }
+
+    public ContatoRequest(String nome, String email, String telefone) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Contato toEntity() {
+        Contato contato = new Contato();
+        BeanUtils.copyProperties(this, contato);
+        return contato;
+    }
 }
